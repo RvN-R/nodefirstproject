9652ad450e5b025f8bb48a698313fe6fc34d1d2f
Richard Van Nairn <rvannairn@googlemail.com>
2022-03-19 16:47:22 +0000

    Using path module

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 
-var logger = require('./logger');
+const logger = require('./logger');
 
 logger.log('message');
+
+const path = require('path');
+
+var pathObj = path.parse(__filename);
+
+console.log(pathObj);
